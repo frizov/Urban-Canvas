35e3ac7f5d99975224d7745b7ffd95aeb01b001e
Filip <filiprizov@gmail.com>
2026-07-01 11:57:27 +0200

    Initial Xcode project setup.

---
+++ Urban-Canvas/Urban-Canvas/Urban_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Urban_CanvasApp.swift
+//  Urban-Canvas
+//
+//  Created by apprenant102 on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Urban_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
